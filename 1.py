dbaaa07f91eb7ca6b4cfb3c6d0d45ade7cc07ac0
Bahram <ishemkulov21@gmail.com>
2022-03-15 20:44:16 +0600

    first commit

---
+++ 1.py
@@ -0,0 +1,21 @@
+name = 'name:Bahram'
+age = 'age:18'
+surname ='surname:Ishemkulov'
+gender =  'gender:male'
+place_of_work = 'place of work:out of employment'
+cource = 'cource:It school Megacom'
+direction ='direction:communication networks'
+have_you_studied_programming_before ='have you studied programming before:did not study'
+salary = 'salary:Polytechnic College'
+disired_salary = 'disired salary:80+k dollars a year'
+
+print(name)
+print(age)
+print(surname)
+print(gender)
+print(place_of_work)
+print(cource)
+print(direction)
+print(have_you_studied_programming_before)
+print(salary)
+print(disired_salary)
